b0dc90af967f496f8f1fd2ea04c22e74a9aa3bb2
hisuihisui <ruru.apple.1212@gmail.com>
2023-09-10 18:49:55 +0900

    Initial Commit

---
+++ src/components/addUser.tsx
@@ -0,0 +1,121 @@
+// input タグのonChangeを使うためにServer Componentにする
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function AddUser() {
+  const router = useRouter();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+  const [profile, setProfile] = useState("");
+
+  // Userテーブルへデータを書き込む
+  // 注意：emailが重複禁止となっている
+  const fetchAsyncAddUser = async () => {
+    // 入力されていないものがあれば、登録しない
+    if (name == "" || email == "" || postTitle == "" || profile == "") {
+      console.log("すべての項目を埋めてください");
+      return;
+    }
+
+    // APIのURL
+    const url = "http://localhost:3000/api/user";
+    // リクエストパラメータ
+    const params = {
+      method: "POST",
+      // JSON形式のデータのヘッダー
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // リクエストボディ
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        postTitle: postTitle,
+        profile: profile,
+      }),
+    };
+
+    // APIへのリクエスト
+    await fetch(url, params);
+
+    // 入力値を初期化
+    setName("");
+    setEmail("");
+    setPostTitle("");
+    setProfile("");
+
+    // 画面をリフレッシュ
+    router.refresh();
+  };
+
+  // inputタグのvalueに変化があった際に実行される
+  const changeNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  // inputタグのvalueに変化があった際に実行される
+  const changeEmailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  // inputタグのvalueに変化があった際に実行される
+  const changePostTitleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPostTitle(e.target.value);
+  };
+
+  // inputタグのvalueに変化があった際に実行される
+  const changeProfileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProfile(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Add User</h2>
+      <div>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={changeNameInput}
+          />
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={changeEmailInput}
+          />
+        </div>
+        <div>
+          <label>Post Title:</label>
+          <input
+            type="text"
+            name="postTitle"
+            value={postTitle}
+            onChange={changePostTitleInput}
+          />
+        </div>
+        <div>
+          <label>Profile:</label>
+          <input
+            type="text"
+            name="profile"
+            value={profile}
+            onChange={changeProfileInput}
+          />
+        </div>
+        <div>
+          <button onClick={fetchAsyncAddUser}>追加</button>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/userList.tsx
@@ -0,0 +1,21 @@
+export default async function UserList() {
+  // APIのURL
+  const url = "http://localhost:3000/api/user";
+  // APIへリクエスト
+  const res = await fetch(url);
+  // レスポンスボディを取り出す
+  const data = await res.json();
+
+  return (
+    <div>
+      <h2>All Users</h2>
+      {data.map((user: any, index: any) => (
+        <div key={index}>
+          Name: {user.name} Email: {user.email} Posts:
+          {user.posts.map((value: any) => `${value.title},`)} Profile:
+          {user.profile?.bio}
+        </div>
+      ))}
+    </div>
+  );
+}
