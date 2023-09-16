e4cd106481441379acb5cb9d1af6b04176d14b61
hisuihisui <ruru.apple.1212@gmail.com>
2023-09-16 23:54:32 +0900

    update

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -2,23 +2,25 @@ export default async function UserList() {
   // APIのURL
   const url = "http://" + process.env.NEXT_PUBLIC_VERCEL_URL + "/api/user";
   // APIへリクエスト
-  const res = await fetch(url, {
+  await fetch(url, {
     cache: "no-store",
-  });
-  // console.log(res.json());
+  })
+  .then((res) => res.json())
+  .then((json) => console.log(json));
+  // console.log(res.status);
   // レスポンスボディを取り出す
-  const data = await res.json();
+  // const data = await res.json();
 
   return (
     <div>
-      <h2>All Users</h2>
+      {/* <h2>All Users</h2>
       {data.map((user: any, index: any) => (
         <div key={index}>
           Name: {user.name} Email: {user.email} Posts:
           {user.posts.map((value: any) => `${value.title},`)} Profile:
           {user.profile?.bio}
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
