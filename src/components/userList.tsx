58991ff74c4566cea4daee2c4d7e4613e77ac441
hisuihisui <ruru.apple.1212@gmail.com>
2023-09-17 11:30:21 +0900

    update

--- src/components/userList.tsx
+++ src/components/userList.tsx
@@ -8,22 +8,26 @@ export default async function UserList() {
     cache: "no-store",
   });
 
-  // レスポンスボディを取り出す
-  const { data } = await res.json();
+  try {
+    // レスポンスボディを取り出す
+    const { data } = await res.json();
 
-  return (
-    <div>
-      <h2>All Users</h2>
-      {data.map((user: any, index: any) => (
-        <div key={index}>
-          <span>Name: {user.name}</span>
-          <span>Email: {user.email}</span>
-          <span>
-            Posts: {user.posts.map((value: any) => `${value.title},`)}
-          </span>
-          <span>Profile: {user.profile?.bio}</span>
-        </div>
-      ))}
-    </div>
-  );
+    return (
+      <div>
+        <h2>All Users</h2>
+        {data.map((user: any, index: any) => (
+          <div key={index}>
+            <span>Name: {user.name}</span>
+            <span>Email: {user.email}</span>
+            <span>
+              Posts: {user.posts.map((value: any) => `${value.title},`)}
+            </span>
+            <span>Profile: {user.profile?.bio}</span>
+          </div>
+        ))}
+      </div>
+    );
+  } catch {
+    return <div></div>;
+  }
 }
