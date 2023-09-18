574cc80cd0835ace986a6b3165fb4f354837c54b
hisuihisui <ruru.apple.1212@gmail.com>
2023-09-18 18:30:41 +0900

    resolve CORS Error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // 全ての API routes にマッチ
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://sample-prisma-next-app.vercel.app",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,POST",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "XContent-Type",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
